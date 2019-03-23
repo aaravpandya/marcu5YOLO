313decaeba2f06781df4a32ea9456243dac9cb49
marcu5fen1x <pandya.aarav.97@gmail.com>
2019-03-23 15:41:33 +0530

    Add project files.

---
+++ marcu5yolo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace marcu5yolo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
